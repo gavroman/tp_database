138a33b8b7dca6235e91d7f5a36575eca09a5449
gavroman <gavroman1998@gmail.com>
2020-04-04 02:53:47 +0300

    Await db init

--- src/index.js
+++ src/index.js
@@ -1,36 +1,65 @@
-const server = require('./server.js');
-const UserHandlers = require('./handlers/userHandlers.js');
-const ForumHandlers = require('./handlers/forumHandlers.js');
-const PostHandlers = require('./handlers/postHandlers.js');
-const ThreadHandlers = require('./handlers/threadHandlers.js');
-const ServiceHandlers = require('./handlers/serviceHandlers.js');
+const fs = require('fs');
+const {Client} = require('pg');
+const express = require('express');
 
-const app = server.app;
-const db = server.db;
+(async () => {
+    const db = new Client({
+        host: 'localhost',
+        port: 5432,
+        user: 'tp_bd_user',
+        database: 'tp_bd',
+        password: 'bazulika',
+    });
 
-let userHandlers = new UserHandlers(db);
-app.post('/api/user/:nickname/create', userHandlers.createUser);
-app.get('/api/user/:nickname/profile', userHandlers.getUser);
-app.post('/api/user/:nickname/profile', userHandlers.updateUser);
+    const sqlInitQueries = fs.readFileSync('./init.sql').toString();
 
-let forumHandlers = new ForumHandlers(db);
-app.post('/api/forum/create', forumHandlers.createForum);
-app.post('/api/forum/:slug/create', forumHandlers.createForumThread);
-app.get('/api/forum/:slug/details', forumHandlers.getForumDetails);
-app.get('/api/forum/:slug/threads', forumHandlers.getForumThreads);
-app.get('/api/forum/:slug/users', forumHandlers.getForumUsers);
+    try {
+        await db.connect();
+        console.log('Database connected');
+        await db.query({text: sqlInitQueries});
+        console.log('Database inited');
+    } catch (err) {
+        console.error('Initialization error', err.stack);
+    }
 
-let postHandlers = new PostHandlers(db);
-app.get('/api/post/:id/details', postHandlers.getPosts);
-app.post('/api/post/:id/details', postHandlers.updatePost);
+    const app = express();
+    app.use(express.json()); // for parsing application/json
 
-let threadHandlers = new ThreadHandlers(db);
-app.post('/api/thread/:slug_or_id/create', threadHandlers.createThreadPosts);
-app.get('/api/thread/:slug_or_id/details', threadHandlers.getThreadDetails);
-app.post('/api/thread/:slug_or_id/details', threadHandlers.updateThreadDetails);
-app.get('/api/thread/:slug_or_id/posts', threadHandlers.getThreadPosts);
-app.post('/api/thread/:slug_or_id/vote', threadHandlers.voteThread);
+    const port = 5001;
+    app.listen(port, () => {
+        console.log('Listening on port', port);
+    });
 
-let serviceHandlers = new ServiceHandlers(db);
-app.post('/api/service/clear', serviceHandlers.clearAllData);
-app.get('/api/service/status', serviceHandlers.getInfo);
+    const UserHandlers = require('./handlers/userHandlers.js');
+    const ForumHandlers = require('./handlers/forumHandlers.js');
+    const PostHandlers = require('./handlers/postHandlers.js');
+    const ThreadHandlers = require('./handlers/threadHandlers.js');
+    const ServiceHandlers = require('./handlers/serviceHandlers.js');
+
+    let userHandlers = new UserHandlers(db);
+    app.post('/api/user/:nickname/create', userHandlers.createUser);
+    app.get('/api/user/:nickname/profile', userHandlers.getUser);
+    app.post('/api/user/:nickname/profile', userHandlers.updateUser);
+
+    let forumHandlers = new ForumHandlers(db);
+    app.post('/api/forum/create', forumHandlers.createForum);
+    app.post('/api/forum/:slug/create', forumHandlers.createForumThread);
+    app.get('/api/forum/:slug/details', forumHandlers.getForumDetails);
+    app.get('/api/forum/:slug/threads', forumHandlers.getForumThreads);
+    app.get('/api/forum/:slug/users', forumHandlers.getForumUsers);
+
+    let postHandlers = new PostHandlers(db);
+    app.get('/api/post/:id/details', postHandlers.getPosts);
+    app.post('/api/post/:id/details', postHandlers.updatePost);
+
+    let threadHandlers = new ThreadHandlers(db);
+    app.post('/api/thread/:slug_or_id/create', threadHandlers.createThreadPosts);
+    app.get('/api/thread/:slug_or_id/details', threadHandlers.getThreadDetails);
+    app.post('/api/thread/:slug_or_id/details', threadHandlers.updateThreadDetails);
+    app.get('/api/thread/:slug_or_id/posts', threadHandlers.getThreadPosts);
+    app.post('/api/thread/:slug_or_id/vote', threadHandlers.voteThread);
+
+    let serviceHandlers = new ServiceHandlers(db);
+    app.post('/api/service/clear', serviceHandlers.clearAllData);
+    app.get('/api/service/status', serviceHandlers.getInfo);
+})();
